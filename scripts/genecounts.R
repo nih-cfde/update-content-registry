9dc6c3ea6c56b412b544e33cc914241aa9b11e1e
Rayna M Harris <raynamharris@users.noreply.github.com>
2022-08-23 16:50:02 -0700

    new gene lists

--- scripts/genecounts.R
+++ scripts/genecounts.R
@@ -16,8 +16,9 @@ genes <- read.csv("~/Downloads/Gene (4).csv") %>%
   inner_join(., ref) %>%
   select(id) %>%
   arrange(id)
-write.table(genes, "../data/inputs/STAGING_PORTAL__available_genes__2022-08-19.txt",
-            col.names = F, row.names = F, quote = F)
+
+#write.table(genes, "../data/inputs/STAGING_PORTAL__available_genes__2022-08-19.txt",
+#            col.names = F, row.names = F, quote = F)
 
 genes <- read.table("../data/inputs/STAGING_PORTAL__available_genes__2022-08-19.txt") %>%
   mutate(ENSEMBL_ID = V1, 
@@ -35,7 +36,7 @@ metgene <- read.table("../data/inputs/gene_IDs_for_MetGene.txt") %>%
 disease <- read.table("../data/inputs/gene_IDs_withdisease.txt") %>%
   mutate(disease = "Diseases")
 
-gtexwidget <- read.table("../data/inputs/gene_IDs_for_expression_widget.txt") %>%
+gtexwidget <- read.table("../data/inputs/gene_IDs_for_gtex.txt") %>%
   mutate(gtexwidget = "GTExWidget")
 
 df <- full_join(genes, kg) %>%
@@ -48,7 +49,7 @@ df <- full_join(genes, kg) %>%
          disease = replace_na(disease, "________"),
          metgene = replace_na(metgene, "____________"),
          gtexwidget = replace_na(gtexwidget, "__________"),
-         markdown = paste(aliases, lincs, kg, disease, metgene, gtexwidget,  sep = " ")) %>%
+         markdown = paste(aliases, lincs, kg, metgene, gtexwidget, disease,  sep = " ")) %>%
   select(ENSEMBL_ID, markdown) %>%
   as_tibble() %>%
   distinct() %>%
@@ -62,20 +63,17 @@ count(df, markdown) %>%
 length(df$ENSEMBL_ID)
 
 df %>%
-  filter(markdown == "LincsReverseSearch KnowledgeGraph Diseases Metabolomics") %>%
-  pull(ENSEMBL_ID)
+  filter(markdown == "Aliases LincsReverseSearch KnowledgeGraph Metabolomics GTExWidget Diseases") %>%
+  pull(ENSEMBL_ID) %>%
+  head()
 
-df %>%
-  filter(markdown != "__________________ ______________ ________ ____________") %>%
-  count()
+#
+l1 <- inner_join(genes, kg) %>% select(ENSEMBL_ID) 
+#write.table(l1, "../data/inputs/gene_IDs_for_gene_kg.txt", row.names = F, col.names = F, quote = F)
 
+l2 <- left_join(genes, lincs) %>% select(ENSEMBL_ID) 
+#write.table(l2, "../data/inputs/gene_IDs_for_lincs_reverse_search.txt", row.names = F, col.names = F, quote = F)
 
-df <- inner_join(genes, kg) %>% select(ENSEMBL_ID) 
-write.table(df, "../data/inputs/gene_IDs_for_gene_kg.txt", row.names = F, col.names = F, quote = F)
-
-df <- left_join(genes, lincs) %>% select(ENSEMBL_ID) 
-write.table(df, "../data/inputs/gene_IDs_for_lincs_reverse_search.txt", row.names = F, col.names = F, quote = F)
-
-df <- inner_join(genes, lincs2) %>% select(ENSEMBL_ID) 
-write.table(df, "../data/inputs/gene_IDs_for_lincs_geo_reverse_appyter.txt", row.names = F, col.names = F, quote = F)
+l3 <- inner_join(genes, lincs2) %>% select(ENSEMBL_ID) 
+#write.table(l3, "../data/inputs/gene_IDs_for_lincs_geo_reverse_appyter.txt", row.names = F, col.names = F, quote = F)
 
--- scripts/genes.R
+++ scripts/genes.R
@@ -4,6 +4,8 @@ library(dplyr)
 # get list of genes
 # cd ~/Downloads
 # curl -O https://raw.githubusercontent.com/nih-cfde/knowledge-base-deposit/main/MW/hsa_mgp_geneid_with_kegg_id_ConvertedGeneIDs.txt
+# curl -O https://storage.googleapis.com/gtex_analysis_v9/long_read_data/LORALS_GTEx_v9_ase_quant_results.gencode.txt.gz 
+# gunzip LORALS_GTEx_v9_ase_quant_results.gencode.txt.gz 
 
 # assumes working dir is scripts dir
 
@@ -13,12 +15,24 @@ df2 <- read.table("../data/inputs/STAGING_PORTAL__available_genes__2022-07-13.tx
   rename(ENSEMBL = V1) %>%
   inner_join(., df) %>%
   arrange(ENSEMBL) %>%
-  select(ENSEMBL) 
+  dplyr::select(ENSEMBL) 
 
 head(df2)
 
-write.table(df2, "gene_IDs_for_MetGene.txt", 
-            row.names = F, col.names = F, quote =  F)
+#write.table(df2, "../data/inputs/gene_IDs_for_MetGene.txt", 
+ #           row.names = F, col.names = F, quote =  F)
 
-write.table(head(df2), "gene_IDs_for_MetGene_test.txt", 
-            row.names = F, col.names = F, quote =  F)
+#write.table(head(df2), "../data/inputs/gene_IDs_for_MetGene_test.txt", 
+  #          row.names = F, col.names = F, quote =  F)
+
+
+df3 <- read.table("~/Downloads/LORALS_GTEx_v9_ase_quant_results.gencode.txt", 
+                  header = T) %>%
+  separate(Gene, into = c("ENSEMBL", "Transcript"), sep = "\\.") %>%
+  dplyr::select(ENSEMBL)  %>%
+  arrange(ENSEMBL) %>%
+  inner_join(., df2)
+head(df3)
+
+#write.table(df2, "../data/inputs/gene_IDs_for_gtex.txt", 
+   #         row.names = F, col.names = F, quote =  F)
