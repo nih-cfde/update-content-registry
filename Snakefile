8c783930e29a47a7386895c3765df485e85662c1
Rayna M Harris <raynamharris@users.noreply.github.com>
2022-09-11 23:52:02 -0700

    now with thousands of ucds widgets, high res plots

--- Snakefile
+++ Snakefile
@@ -202,7 +202,7 @@ rule gene_json_ucsc_genome_browser_widget:
     message: "build UCSC genome browser iframe-include for genes"
     input:
         script = "scripts/build-markdown-pieces-ucsc-genome-browser-widget.pl",
-        id_list = "data/inputs/gene_IDs_for_UCSC_genome_browser_widget.txt",
+        id_list = "data/inputs/gene_IDs_for_alias_tables.txt",
         coord_info = "data/inputs/homo_sapiens.coords.tsv",
         validate_csv = expand("data/validate/{term}.csv", term=TERM_TYPES)
     output:
--- logs/plots.R
+++ logs/plots.R
@@ -2,22 +2,23 @@ suppressPackageStartupMessages(library(tidyverse))
 suppressPackageStartupMessages(library(scales))
 
 df <- read.csv("logs/skipped.csv", header = F)  %>%
-  mutate(widget = paste(V2, V1, sep = "\n")) %>%
-  group_by(V1, V2, V4, widget) %>% 
+  mutate(widget = paste(V2, V1, sep = "-")) %>%
+  group_by(V2, widget, V4) %>% 
   summarize(count = n())
+head(df)
 
 p1 <- ggplot(df, aes(x = widget, y = count, fill = V2)) +
   geom_bar(stat = "identity") +
-  scale_y_log10() +
   coord_flip() +
-  facet_wrap(~V4) +
+  facet_wrap(~V4, nrow = 1) +
   labs(subtitle = "Number of input IDs not present in alias or reference (ref) files", fill = "") +
-  geom_text(aes(label = count), hjust = 0, size = 3) +
+  geom_text(aes(label = count), hjust = 0.5, size = 3) +
   theme(legend.position = "bottom")
+p1
 
 print("Saving plot of skipped IDs.")
 
-png(file = "logs/skipped.png")
+png(file = "logs/skipped.png", res = 300, width = 7, height = 4, units = "in")
 plot(p1)
 dev.off()
 
@@ -25,20 +26,21 @@ df2 <- read.csv("logs/chunks.csv",
                 header = F) %>%
   separate(V1, into = c("out", "piece","dir"), sep = "_") %>%
   separate(dir, into = c("term", "widget"), sep = "/") %>%
-  select(term, widget, V2) %>%
-  mutate(V1 = paste(term, widget, sep = "-"))
+  mutate(V1 = paste(term, widget, sep = "-")) %>%
+  select(term, V1, V2) 
+head(df2)
 
 p2 <- ggplot(df2, aes(x = V1, y = V2, fill = term)) +
   geom_bar(stat = "identity") +
   scale_y_continuous(labels = scales::comma) +
   theme(legend.position = "bottom") +
   coord_flip() +
-  geom_text(aes(label = V2), hjust = 0, size = 3) +
+  geom_text(aes(label = V2), hjust = 0.5, size = 3) +
   labs(subtitle = "Number of json files generated", fill = "", y = "Count", x = "Widget")
-
+p2
 
 print("Saving plot of json files generated.")
 
-png(file = "logs/chunks.png")
+png(file = "logs/chunks.png", res = 300, width = 7, height = 4, units = "in")
 plot(p2)
 dev.off()
