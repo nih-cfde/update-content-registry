6e1518f242e1b7d7436fe2b4325f75f5b326c5bb
Rayna M Harris <raynamharris@users.noreply.github.com>
2022-10-24 18:28:54 -0700

    update drug central

--- scripts/build-compound-drugcentral.py
+++ scripts/build-compound-drugcentral.py
@@ -38,7 +38,7 @@ def main():
     if not os.path.exists(output_dir):
         os.mkdir(output_dir)
 
-    ref_file = cfde_common.REF_FILES.get(term)
+    ref_file = cfde_common.ID_FILES.get(term)
     if ref_file is None:
         print(f"ERROR: no ref file for term. Dying terribly.", file=sys.stderr)
         sys.exit(-1)
@@ -89,8 +89,7 @@ def main():
             line = line.strip()
             if line:
                 if line not in ref_id_list:
-                    print(f"ERROR: requested input id {line} not found in ref_id_list", file=sys.stderr)
-                    sys.exit(-1)
+                    print(f"WARNING: requested input id {line} not found in ref_id_list", file=sys.stderr)
 
                 id_list.add(line)
 
